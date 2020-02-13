2f8f9ae8ca571be9368aa6274b58aa44739a8ff8
ladmorph <ladmorph@gmail.com>
2020-02-13 09:53:50 +0300

    fix

--- src/main/java/com/example/currencyConverter/controllers/LoginController.java
+++ src/main/java/com/example/currencyConverter/controllers/LoginController.java
@@ -1,41 +1,20 @@
 package com.example.currencyConverter.controllers;
 
-
-import com.example.currencyConverter.entities.User;
 import com.example.currencyConverter.services.UserService;
 import com.example.currencyConverter.services.impl.UserServiceImpl;
-import com.example.currencyConverter.utils.CurrencyXmlHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @Autowired
-    UserService userService;
-
-
-    @GetMapping("/login")
-    public String greetings() {
-        return "loginForm";
+    @PostMapping("/login")
+    public String auth(@RequestParam("username") String username, @RequestParam("password") String password) {
+        UserService userService = new UserServiceImpl();
+        if (userService.checkUserByUsernameAndPassword(username, password))
+            return "converter";
+        else
+            return "loginForm";
     }
-
-    @PostMapping(value = "/loginHandler")
-    public ModelAndView handleLoginRequest(@ModelAttribute("loginForm") @Valid User loginForm, ModelMap model) {
-
-        if (userService.checkUserByUsernameAndPassword(loginForm.getLogin(), loginForm.getPassword())) {
-            return new ModelAndView(new RedirectView("/login"));
-        } else {
-            return new ModelAndView(new RedirectView("/converter"));
-        }
-    }
-
 }
