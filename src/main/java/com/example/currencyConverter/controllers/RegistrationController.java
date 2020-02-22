b208c068f9e638a18f708dd5cb6de6e28370e90d
ladmorph <ladmorph@gmail.com>
2020-02-22 09:30:08 +0300

    completion of registration

--- src/main/java/com/example/currencyConverter/controllers/RegistrationController.java
+++ src/main/java/com/example/currencyConverter/controllers/RegistrationController.java
@@ -7,7 +7,6 @@ import com.example.currencyConverter.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,14 +24,14 @@ public class RegistrationController {
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String showRegistrationForm(Model model) {
-
         model.addAttribute("user", new User());
         return "registration";
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String showRegistrationForm(@ModelAttribute("user") User user, BindingResult bindingResult) {
-        if (userService.checkUserByUsername(user.getUsername())) {
+    public String showRegistrationForm(@ModelAttribute("user") User user, Model model) {
+        if (userService.checkUserByUsername(user.getUsername())) { // checking the presence of a user in the database
+            model.addAttribute("error", true);
             return "registration";
         } else {
             user.setRoles(Collections.singleton(Role.USER));
